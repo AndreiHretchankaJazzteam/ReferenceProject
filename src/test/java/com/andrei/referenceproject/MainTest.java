b8e3927c7f7415d242186e6755983efba901bc60
andrei.hretchanka.jazzteam@gmail.com <andrei.hretchanka.jazzteam@gmail.com>
2023-07-05 14:46:49 +0300

    add some for test jenkins

--- src/test/java/com/andrei/referenceproject/MainTest.java
+++ src/test/java/com/andrei/referenceproject/MainTest.java
@@ -1,4 +1,13 @@
 package com.andrei.referenceproject;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MainTest {
+    @Test
+    public void updateTest() {
+
+        assertTrue(true);
+    }
 }
