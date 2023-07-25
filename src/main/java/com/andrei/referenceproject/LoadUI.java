fab0efb1ae34492d9da9f81f35b4b9b0e22aeb74
andrei.hretchanka.jazzteam@gmail.com <andrei.hretchanka.jazzteam@gmail.com>
2023-07-25 15:42:15 +0300

    PHASE7 - fix bug related profiles in tests

--- src/main/java/com/andrei/referenceproject/LoadUI.java
+++ src/main/java/com/andrei/referenceproject/LoadUI.java
@@ -2,10 +2,12 @@ package com.andrei.referenceproject;
 
 import com.andrei.referenceproject.gui.frame.MainFrame;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("!test")
 public class LoadUI implements ApplicationListener<ContextRefreshedEvent> {
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
